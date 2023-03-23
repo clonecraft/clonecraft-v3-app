032f356258893bfedbf207f2780a5107bb6a3b81
elixirevo <elixirevocation@gmail.com>
2023-03-23 21:27:15 +0900

    fix

--- src/pages/equip/index.svelte
+++ src/pages/equip/index.svelte
@@ -4,9 +4,9 @@
   import V3List from '@/components/v3-list/index.svelte'
   import AssetList from '@/components/asset-list/index.svelte'
 
-  const content = `CxNxD Omega는 Nectar War 게임에 사용되는 NFT입니다. 민팅 가격은 1개에 2 klay 입니다.
-하루에 총 1000개 민팅이 가능하며 지갑당 하루에 9개 민팅 가능합니다. CxNxD Omega는 민팅시 스킬을 랜덤으로 부여받습니다.
-또한 CxNxD Asset NFT를 활용하여 CxNxD Omega의 의상을 바꿀 수 있습니다.`
+  const content = `CxNxD Omega는 Clonecraft 게임에 사용될 예정인 NFT입니다. 
+CxNxD Asset Nft를 사용해서 Omega nft의 이미지와 메타데이터를 변경할 수 있습니다. 
+Asset 장착과 해제엔 Amber 토큰으로 수수료가 발생합니다.`
 </script>
 
 <div class="wrap">
